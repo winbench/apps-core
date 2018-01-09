a1e9073d856bd03469159afa518de3c9e94fb32f
Tobias Kiertscher <kiertscher@th-brandenburg.de>
2018-01-09 12:38:28 +0100

    fixed custom setup script for RubyGems

--- scripts/bench/rubygems.setup.ps1
+++ scripts/bench/rubygems.setup.ps1
@@ -1,6 +1,6 @@
 $ruby = App-Exe "Bench.Ruby"
 $gemsTmpDir = App-Dir "Bench.RubyGems"
 
-$packageDir = gci "$gemsTmpDir\rubygems-*" | Sort-Object -Descending
+$packageDir = gci "$gemsTmpDir\rubygems-*" | Sort-Object -Descending | Select-Object -First 1
 cd $packageDir
 & $ruby setup.rb
